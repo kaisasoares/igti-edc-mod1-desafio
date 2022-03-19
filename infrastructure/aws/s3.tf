26c478e351be4fc2a392d138201a397d48a974b5
Kaisa Soares <kaisasoares@hotmail.com>
2022-03-19 17:43:12 -0300

    adicionando arquivos de infra e scripts

---
+++ infrastructure/aws/s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "datalake" {
+  bucket = "datalake-igti-edc-kass-tf"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+}
+
+resource "aws_s3_bucket_acl" "datalake" {
+  bucket = aws_s3_bucket.datalake.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "datalake" {
+  bucket = aws_s3_bucket.datalake.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
