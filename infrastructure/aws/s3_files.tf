c8ff5f24852e03b78999bb07d349508678c0906a
Kaisa Soares <kaisasoares@hotmail.com>
2022-03-19 18:51:04 -0300

    corrigindo caminho do job spark ao usar o filemd5

--- infrastructure/aws/s3_files.tf
+++ infrastructure/aws/s3_files.tf
@@ -3,6 +3,6 @@ resource "aws_s3_object" "codigo_spark" {
   key    = "emr-code/pyspark/emr_job_spark.py"
   acl    = "private"
   source = "../../etl/emr_job_spark.py"
-  etag   = filemd5("../etl/emr_job_spark.py")
+  etag   = filemd5("../../etl/emr_job_spark.py")
 
 }
